446d72d226a1ad1d17f3c16d3723bf98fc00c2cd
장기영 <iamurgini@naver.com>
2024-02-27 12:58:33 +0900

    2024.02.27 주문오류 체크

--- src/main/java/com/dae/eean/controller/app01/App01CrudController.java
+++ src/main/java/com/dae/eean/controller/app01/App01CrudController.java
@@ -1067,18 +1067,22 @@ public class App01CrudController {
             index02BonsaDto = service02.GetCifBonsa(index02BonsaDto);
             _index03Dto = service03.GetJpumOrderJkey(_index03Dto); //품목정보
             if(_index03Dto == null){
+                log.info("index14Save 재고 없음 =====> index03Dto == null"  );
                 log.info("jfrdate =====> " + frdate );
-                log.info("jkey =====> " + _index03Dto.getJkey() );
-                log.info("jqty =====> " + _index03Dto.getJqty() );
-                log.info("error Exception index14Save=====> GetJpumOrderJkey NULL" );
+                log.info("jmodel =====> " + jmodel );
+                log.info("jcolor =====> " + jcolor );
+                log.info("jcolor =====> " + jcolor );
+                log.info("jbonsa =====> " + jbonsa );
+                log.info("jbonsa2 =====> " + jbonsa2 );
+                log.info("jpbgubn =====> " + jpbgubn );
                 return "error";
             }
             _ll_jqty = Integer.parseInt(_index03Dto.getJqty());
             if(_ll_jqty == 0 || _ll_jqty == null){
+                log.info("index14Save 재고 없음 =====> _ll_jqty == 0"  );
                 log.info("jfrdate =====> " + frdate );
                 log.info("jkey =====> " + _index03Dto.getJkey() );
                 log.info("jqty =====> " + _index03Dto.getJqty() );
-                log.info("index14Save 재고 없음 =====> " + _index03Dto.getJqty() );
                 return "jaego";
             }
             indexDa023Dto.setCltcd(acode);
@@ -1475,10 +1479,10 @@ public class App01CrudController {
             index02BonsaDto = service02.GetCifBonsa(index02BonsaDto);
             _index03Dto = service03.GetJpumOrderJkey02(_index03Dto); //품목
             if(_index03Dto == null){
+                log.info("error Exception index14SaveCust =====> _index03Dto NULL" );
                 log.info("jfrdate =====> " + frdate );
-                log.info("jkey =====> " + _index03Dto.getJkey() );
+                log.info("jkey =====> " + jkey);
                 log.info("jqty =====> " + _index03Dto.getJqty() );
-                log.info("error Exception index14SaveCust =====> GetJpumOrderJkey NULL" );
                 return "error";
             }
             _ll_jqty = Integer.parseInt(_index03Dto.getJqty());
