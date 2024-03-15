990cfebceb1b62293160a1553116b3d337902dbc
장기영 <iamurgini@naver.com>
2024-03-15 11:17:51 +0900

    2024.03.15예약->주문 makflag 등록

--- src/main/java/com/dae/eean/controller/app01/App01Controller.java
+++ src/main/java/com/dae/eean/controller/app01/App01Controller.java
@@ -17,15 +17,13 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+
 
 // @RestController : return을 텍스트로 반환함.
 @Controller
